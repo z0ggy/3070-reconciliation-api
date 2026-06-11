e4edd83749f39e0ce622aaa9545ec2225ae6c1cf
z0ggy <zoggy76@gmail.com>
2026-06-11 21:55:42 +0100

    test: add normalized string match test

--- tests/test_matching_engine.py
+++ tests/test_matching_engine.py
@@ -1,6 +1,11 @@
-from src.utils.match import match_engine
+from src.utils.match import normalize_text
 
 
 def test_exact_city_match():
     city = "Dublin"
-    assert match_engine(city) == "Dublin"
+    assert normalize_text(city) == "dublin"
+
+
+def test_unormalized_exatct_city_match():
+    city = "  DUblin  "
+    assert normalize_text(city) == "dublin"
