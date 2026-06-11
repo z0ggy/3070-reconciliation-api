bf89a958748a61eedc793ccd6bce40083476f800
z0ggy <zoggy76@gmail.com>
2026-06-11 21:55:01 +0100

    feat: change name match_engine -> normalize_text add lowercase and stripe spaces

--- src/utils/match.py
+++ src/utils/match.py
@@ -1,2 +1,21 @@
-def match_engine(city: str):
-    return city
+from pathlib import Path
+import json
+
+"""
+References:
+    - https://stackoverflow.com/questions/10018679/python-find-closest-string-from-a-list-to-another-string
+    - https://docs.python.org/3/library/difflib.html
+"""
+DATA_PATH = Path(__file__).parent / "data" / "geo_data.json"
+
+
+def load_file() -> dict:
+    with open(DATA_PATH, "r", encoding="utf-8") as file:
+        return json.load(file)
+
+
+def normalize_text(text: str):
+    """
+    Normalize text
+    """
+    return text.lower().strip()
