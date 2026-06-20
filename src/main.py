bae7845436bcda195ee934d48f5107d2ee1bb6df
z0ggy <zoggy76@gmail.com>
2026-06-20 10:29:47 +0100

    feat(api): add query as parameter

--- src/main.py
+++ src/main.py
@@ -2,9 +2,14 @@ from fastapi import FastAPI
 
 from src.utils.match import match_data
 
+"""
+References:
+    - https://pypi.org/project/fastapi/
+    - https://fastapi.tiangolo.com/
+"""
 app = FastAPI(
     title="Reconciliation API prototype",
-    description="Prototype API for matching messy geographic names standarised Irish geographics entities.",
+    description="Prototype API for matching messy geographic names standarised Irish geographic entities.",
     version="0.1.0",
 )
 
@@ -14,7 +19,7 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/reconcile")
-def reconcile():
-    candidates = match_data("Dublin")
+@app.get("/reconcile/{q}")
+def reconcile(q: str):
+    candidates = match_data(query=q)
     return candidates
