c2a22b80d5b9ff06827d493c3062af8915cd3cb2
z0ggy <zoggy76@gmail.com>
2026-07-02 16:39:15 +0100

    tests: parametrise and add more test cases

--- tests/test_normaliser.py
+++ tests/test_normaliser.py
@@ -1,30 +1,65 @@
-from src.normalisation import PlaceNameNormaliser
+import pytest
+
+from src.place_normaliser import PlaceNameNormaliser
+
+"""
+References:
+    - https://docs.pytest.org/en/7.1.x/example/parametrize.html
+    - https://docs.pytest.org/en/stable/
+"""
+
+punctuation_data = [
+    ("Dublin, Ireland", "dublin ireland"),
+    ("Dublin. Ireland!?", "dublin ireland"),
+]
+
+accent_data = [
+    ("Dún-Laoghaire", "dun laoghaire"),
+    ("Cill Chomáin Mhór Theas ", "cill chomain mhor theas"),
+]
+
+dash_data = [
+    ("Dublin-City", "dublin city"),
+    ("Galway-Co.", "galway county"),
+]
+
+space_data = [
+    ("   Dublin   !!!", "dublin"),
+    ("!!@    Cork ", "cork"),
+]
+
+county_data = [
+    ("Co.!!! Dublin city", "county dublin city"),
+    ("Galway  !!! Co.", "galway county"),
+]
 
 # initialise instance of normaliser
 place_normaliser = PlaceNameNormaliser()
 normaliser = place_normaliser.normalise
 
 
-def test_remove_punctuation():
-    city = "Dublin, Ireland"
-    assert normaliser(city) == "dublin ireland"
+@pytest.mark.parametrize("city, expected", punctuation_data)
+def test_punctuation(city, expected):
+    assert normaliser(city) == expected
 
 
-def test_remove_accent():
-    city = "Dún-Laoghaire"
-    assert normaliser(city) == "dun laoghaire"
+@pytest.mark.parametrize("city, expected", accent_data)
+def test_remove_accent(city, expected):
+    assert normaliser(city) == expected
 
 
-def test_dash_replace():
-    city = "Dublin-City"
-    assert normaliser(city) == "dublin city"
+@pytest.mark.parametrize("city, expected", dash_data)
+def test_dash_replace(city, expected):
+    # found bug "Galway-Co." become "galway co" instead "galway county"
+    # fix by re-arrange normalisation pipeline
+    assert normaliser(city) == expected
 
 
-def test_remove_extra_spaces():
-    city = "   Dublin   !!!"
-    assert normaliser(city) == "dublin"
+@pytest.mark.parametrize("city, expected", space_data)
+def test_remove_extra_spaces(city, expected):
+    assert normaliser(city) == expected
 
 
-def test_convert_county():
-    city = "Co. Dublin"
-    assert normaliser(city) == "county dublin"
+@pytest.mark.parametrize("city, expected", county_data)
+def test_convert_county(city, expected):
+    assert normaliser(city) == expected
