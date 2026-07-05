e63a71a5d2f6d750f50dfb80f60fd6cf7c89c896
z0ggy <zoggy76@gmail.com>
2026-07-05 11:34:12 +0100

    refactor: matcher add functions

--- src/matcher.py
+++ src/matcher.py
@@ -40,45 +40,77 @@ class Matcher:
             # store name and aliases
             names_to_check = [place["name"]] + place.get("aliases", [])
 
-            # store strongest match (name or aliases)
-            best_score = 0.0
+            # find best score
+            best_score, matched_on, matched_value = self.find_best_score(
+                place, names_to_check, normalised_query
+            )
+            # assign and filter weak candidates
+            candidates = self.filter_weak_candidate(
+                place, best_score, matched_on, matched_value
+            )
 
-            # represent a match 'source' (name or alias)
-            matched_on = "name"
-
-            # actual name or alias (what was matched)
-            matched_value = place["name"]
-
-            for i, candidate_name in enumerate(names_to_check):
-                # normalise each candidate
-                normalised_candidate = self.normaliser.normalise(candidate_name)
-
-                # calculate score
-                score = self.scorer.calculate_score(
-                    normalised_query, normalised_candidate
-                )
-
-                # logic for score result
-                if score > best_score:
-                    best_score = score
-                    matched_on = "name" if i == 0 else "alias"
-                    matched_value = candidate_name
-
-            # filter for weak candidates
-            if best_score > self.min_score:
-                candidates.append(
-                    {
-                        "id": place["id"],
-                        "name": place["name"],
-                        "type": place["type"],
-                        "country": place["country"],
-                        "score": round(best_score, 3),
-                        "matched_on": matched_on,
-                        "matched_value": matched_value,
-                    }
-                )
         # sort candidates descending (highest first)
         candidates.sort(key=lambda item: item["score"], reverse=True)
 
         # return a list of candidates highest first up to limit
         return candidates[:limit]
+
+    def find_best_score(self, place: dict, names_to_check: list, normalised_query: str):
+        """Find the best score for place using calculate_score logic
+        place
+        """
+        if not place:
+            raise ValueError("Place should be not empty")
+
+        if not names_to_check:
+            raise ValueError("Names to check should be not empty")
+
+        if not normalised_query:
+            raise ValueError("Normalised query should be not empty")
+
+        # store strongest match (name or aliases)
+        best_score = 0.0
+
+        # represent a match 'source' (name or alias)
+        matched_on = "name"
+
+        # actual name or alias (what was matched)
+        matched_value = place["name"]
+
+        for i, candidate_name in enumerate(names_to_check):
+            # normalise each candidate
+            normalised_candidate = self.normaliser.normalise(candidate_name)
+
+            # calculate score
+            score = self.scorer.calculate_score(normalised_query, normalised_candidate)
+
+            # logic for score result
+            if score > best_score:
+                best_score = score
+                matched_on = "name" if i == 0 else "alias"
+                matched_value = candidate_name
+
+        return best_score, matched_on, matched_value
+
+    def filter_weak_candidate(
+        self, place, best_score, matched_on, matched_value
+    ) -> list[dict]:
+        """Weak candidate filter"""
+
+        # store candidates
+        candidates: list[dict] = []
+
+        # filter for weak candidates logic
+        if best_score > self.min_score:
+            candidates.append(
+                {
+                    "id": place["id"],
+                    "name": place["name"],
+                    "type": place["type"],
+                    "country": place["country"],
+                    "score": round(best_score, 3),
+                    "matched_on": matched_on,
+                    "matched_value": matched_value,
+                }
+            )
+        return candidates
