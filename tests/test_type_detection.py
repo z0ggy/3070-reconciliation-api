634f6f9b3c0012f4b0f51262d1c6bf4a50230d22
z0ggy <zoggy76@gmail.com>
2026-07-16 21:48:55 +0100

    test: add more type_detection test after normalisation

--- tests/test_type_detection.py
+++ tests/test_type_detection.py
@@ -12,3 +12,29 @@ normaliser = place_normaliser.normalise
 @pytest.mark.parametrize("normalised_query, expected_type", place_type_data)
 def test_type_detection(normalised_query, expected_type):
     assert type_detection(normalised_query) == expected_type
+
+
+def test_detect_county_abbreviation_after_normalisation():
+    normalised_query = normaliser("Co. Dublin")
+
+    assert normalised_query == "county dublin"
+    assert type_detection(normalised_query) == "county"
+
+
+def test_detect_county_suffix_after_normalisation():
+    normalised_query = normaliser("Galway Co.")
+
+    assert normalised_query == "galway county"
+    assert type_detection(normalised_query) == "county"
+
+
+def test_council_has_priority_over_city():
+    assert type_detection("dublin city council") == "local_authority"
+
+
+def test_council_has_priority_over_county():
+    assert type_detection("dlr county council") == "local_authority"
+
+
+def test_detect_city_and_county_conflict():
+    assert type_detection("dublin city county") is None
