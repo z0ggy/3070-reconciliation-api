10896864a7592fb0f6b7840a52eb3518ed15c48e
z0ggy <zoggy76@gmail.com>
2026-07-22 21:51:59 +0100

    refactor: simplify entity type detection

--- src/type_detection.py
+++ src/type_detection.py
@@ -3,6 +3,7 @@ from typing import Literal, TypeAlias
 """
 References:
     - https://typing.python.org/en/latest/spec/aliases.html
+    - https://www.w3schools.com/python/ref_set_issubset.asp
 """
 
 EntityType: TypeAlias = Literal["city", "county", "local_authority"]
@@ -14,10 +15,14 @@ def type_detection(normalised_query: str) -> EntityType | None:
         return None
 
     # Check whole word instead of just substring in normalised_query
-    words = normalised_query.split()
+    words = set(normalised_query.split())
+
+    # Return True/False if all items in set x are present in set y
+    is_local_authority = {"local", "authority"}.issubset(words)
 
     # Prioritise "council" and "local authority" over "city" or "county."
-    if "council" in words or ("local" in words and "authority" in words):
+    # if "council" in words or ("local" in words and "authority" in words):
+    if is_local_authority:
         return "local_authority"
 
     # Handle conflicted type like "dublin city county"
@@ -29,6 +34,10 @@ def type_detection(normalised_query: str) -> EntityType | None:
 
     if "county" in words:
         return "county"
+
+    # Council without city or county context.
+    if "council" in words:
+        return "local_authority"
 
     return None
 
