0bddb86ee07e43b0bf99b6fd185a12a902d8b0a9
z0ggy <zoggy76@gmail.com>
2026-08-15 20:58:52 +0100

    fix: data_loader Basepyright syntax

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,9 +1,16 @@
 import json
 from pathlib import Path
+from typing import cast
 
 from src.geo_types import PlaceRecord
+
+"""
+References:
+    - https://stackoverflow.com/questions/51457563/what-does-typing-cast-do-in-python
+    - https://stackoverflow.com/questions/75010631/python-typing-cast-vs-built-in-casting
+"""
 
 
 def load_json_file(path: Path) -> list[PlaceRecord]:
     with path.open("r", encoding="utf-8") as file:
-        return json.load(file)
+        return cast(list[PlaceRecord], json.load(file))
