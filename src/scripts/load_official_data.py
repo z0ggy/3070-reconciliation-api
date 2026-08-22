b1fc9545aa1eaa94044eb614fe2c0de5fd0787e6
z0ggy <zoggy76@gmail.com>
2026-08-22 12:16:04 +0100

    fix: fix basedpyright syntax

--- src/scripts/load_official_data.py
+++ src/scripts/load_official_data.py
@@ -1,18 +1,19 @@
 import json
 import sys
 from pathlib import Path
+from typing import cast
+
+from typing_extensions import TypedDict
 
 from src.config import BASE_DIR
 
 """
 Replace the manually created prototype dataset with official Irish source data.
+Module can be run with: uv run -m src.scripts.load_official_data
 References:
     - https://www.pythontutorials.net/blog/how-to-use-to-find-files-recursively/
 """
 
-
-# BASE_DIR: Path = Path(__file__).resolve().parents[1]
-print(f"BASEDIR: {BASE_DIR}")
 sys.path.insert(0, str(BASE_DIR))
 
 LOGAINM_DIR: Path = BASE_DIR / "data" / "official" / "logainm"
@@ -31,22 +32,26 @@ def load_logainm_counties() -> list[dict]:  # pyright: ignore[reportUnknownParam
     "countPerPage": 10,
     "results": [
     """
-    print(f"BASE_DIR: {BASE_DIR}")
-    print(f"LOGAINM_DIR: {LOGAINM_DIR}")
+
+    class LogainmType(TypedDict):
+        totalCount: int
+        totalPages: int
+        currentPage: int
+        countPerPage: int
+        results: list[dict[str, object]]
 
     files: list[Path] = sorted(LOGAINM_DIR.glob("counties*.json"))
-    print(f"FILES: {files}")
 
     if not files:
         raise FileNotFoundError(f"No files found in: {LOGAINM_DIR}")
 
-    pages: dict[int, dict] = {}  # pyright: ignore[reportMissingTypeArgument, reportUnknownVariableType]
+    pages: dict[int, LogainmType] = {}
 
     for file_path in files:
         with file_path.open(encoding="utf-8") as file:
-            data = json.load(file)  # pyright: ignore[reportAny]
+            data: LogainmType = cast(LogainmType, json.load(file))
 
-        page_number: int = int(data["currentPage"])  # pyright: ignore[reportAny]
+        page_number: int = int(data["currentPage"])
 
         print(page_number)
 
